65958305997f9ca6432539845663ec0efe58e55e
alectrico <ventas@alectrico.cl>
2025-08-02 20:21:06 -0400

    Extrayendo el challenge

--- src/entry.py
+++ src/entry.py
@@ -46,7 +46,7 @@ async def on_fetch(request, env):
 
     if url.path.startswith("/webhook") and 'hub.mode' in params and 'hub.verify_token' in params:
         if params["hub.mode"] == ['subscribe']: #nd params['hub.verify_token'] == VERIFY_TOKEN
-           return Response(params['hub.challenge'], status=200)
+           return Response(params['hub.challenge'].first, status=200)
         else:
            return Response("Error", status=403)
     else:

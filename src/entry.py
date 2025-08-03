b0b529797aa6a721f46fecab99cd55bb298944e2
alectrico <ventas@alectrico.cl>
2025-08-03 13:45:41 -0400

    Tratando de ver entry[0]

--- src/entry.py
+++ src/entry.py
@@ -50,7 +50,7 @@ async def on_fetch(request, env):
     if url.path.startswith("/webhook") and method == 'POST':
         entry = (await request.json()).entry
         if entry is not None:
-           console.log(f"entry {entry}")
+           console.log(f"entry {entry[0]}")
            value = entry[0]["changes"][0]["value"]
            if value is not None:
              console.log(f"value: {value}")

b57cb8daa6480e024c4d3fe24a44713a109ce36f
alectrico <ventas@alectrico.cl>
2025-08-06 02:10:42 -0400

    Avanzando

--- src/entry.py
+++ src/entry.py
@@ -132,44 +132,42 @@ async def on_fetch(request, env):
         console.log("En webhook")
         request_json = await request.json()
         value = request_json.entry[0].changes[0].value
-        try:
-          console.log("En try")
-          wa_id = request_json.entry[0].changes[0].value.contacts[0].wa_id
-          console.log("wa_id: {wa_id}")
-          response_json = request_json.entry[0].changes[0].value.messages[0].interactive.nfm_reply.response_json
+        console.log("En try")
+        wa_id = request_json.entry[0].changes[0].value.contacts[0].wa_id
+        console.log("wa_id: {wa_id}")
+        response_json = request_json.entry[0].changes[0].value.messages[0].interactive.nfm_reply.response_json
 
-          console.log(f"response_json {response_json}")
-          uri     = f"https://graph.facebook.com/v23.0/{env.PHONE_NUMBER_ID}/messages"
-          #ri     = f"https://www.alectrico.cl/api/v1/santum/webhook"
+        console.log(f"response_json {response_json}")
+        uri     = f"https://graph.facebook.com/v23.0/{env.PHONE_NUMBER_ID}/messages"
+        #ri     = f"https://www.alectrico.cl/api/v1/santum/webhook"
 
-          headers = {
+        headers = {
                 "Content-Type": "application/json",
                 "Authorization": f"Bearer {env.META_USER_TOKEN}"
-          }
-          body = {
+        }
+        body = {
                     "messaging_product" :  "whatsapp",
                     "recipient_type"    :  "individual",
                     "to"                :  wa_id,
                     "type"              :  "text",
                     "text"              :  { "preview_url" : true,
                         "body" : response_json }
-          }
+        }
 
-          options = {
+        options = {
                "body": json.dumps(body),
                "method": "POST",
                "headers": {
                  "Authorization": f"Bearer {env.META_USER_TOKEN}",
                  "content-type": "application/json;charset=UTF-8"
                },
-          }
+        }
 
-          response = await fetch(uri, to_js(options))
-          console.log(f"response {response}")
-          content_type, result = await gather_response(response)
-          return Response.new( response_json, status="200")
-        except:
-            try:
+        response = await fetch(uri, to_js(options))
+        console.log(f"response {response}")
+        content_type, result = await gather_response(response)
+        return Response.new( response_json, status="200")
+        try:
                 mensaje = value.messages[0].text.body 
                 console.log(f"mensaje {mensaje}")
 
@@ -182,7 +180,7 @@ async def on_fetch(request, env):
                 }
                 body ={
                      'messaging_product': 'whatsapp',
-                     'to': '56981370042',
+                     'to': wa_id,
                      'type': 'template',
                      'template': { 'name': 'prueba',
                                    'language': {'code': 'en_US'}

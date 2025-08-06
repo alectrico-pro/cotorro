d24c924eab62af50e7a9f8c59b617985d5db8ebe
alectrico <ventas@alectrico.cl>
2025-08-06 00:25:28 -0400

    de nuevo

--- src/entry.py
+++ src/entry.py
@@ -132,8 +132,8 @@ async def on_fetch(request, env):
         request_json = await request.json()
         value = request_json.entry[0].changes[0].value
         try:
-          request_json = await request.json()
-          wa_id          = request_json.entry[0].changes[0].value.contacts[0].profile.wa_id
+          request_json  = await request.json()
+          wa_id         = request_json.entry[0].changes[0].value.contacts[0].profile.wa_id
           console.log(f"wa_id {wa_id}")
           response_json = request_json.entry[0].changes[0].value.messages[0].interactive.nfm_reply.response_json
           console.log(f"response_json {response_json}")

38e307c18c497d3b5587d2ca261e2d0cd784b29f
alectrico <ventas@alectrico.cl>
2025-08-07 02:14:39 -0400

    denuevo

--- src/entry.py
+++ src/entry.py
@@ -159,7 +159,7 @@ async def on_fetch(request, env):
             console.log("Es un statuses")
             console.log(f"Status: {value.statuses[0].status}")
             if value.statuses[0].status == 'failed':
-               console.log(f"Es failed, error: {value.status[0].status.errors[0].title}" )
+               console.log(f"Es failed, error: {value.statuses[0].status.errors[0].title}" )
             return Response.new('ok', status="200")
         else:
            console.log("No se ha identificado")

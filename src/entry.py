de540f20ff3ae8847be89e4874106e65993c3a87
alectrico <ventas@alectrico.cl>
2025-08-06 22:23:57 -0400

    separando los diferentes mensajes

--- src/entry.py
+++ src/entry.py
@@ -130,48 +130,20 @@ async def on_fetch(request, env):
     #de pago.
     if url.path.startswith("/webhook") and method == 'POST':
         console.log("En webhook")
-        await flow_reply_processor( request, env)
-        return Response.new('ok', status="200")
-
-
-        """
         request_json = await request.json()
+        console.log( f"request_json {request_json}")
         value = request_json.entry[0].changes[0].value
-        console.log("En try")
-        wa_id = request_json.entry[0].changes[0].value.contacts[0].wa_id
-        console.log("wa_id: {wa_id}")
-        response_json = request_json.entry[0].changes[0].value.messages[0].interactive.nfm_reply.response_json
 
-        console.log(f"response_json {response_json}")
-        uri     = f"https://graph.facebook.com/v23.0/{env.PHONE_NUMBER_ID}/messages"
-        #ri     = f"https://www.alectrico.cl/api/v1/santum/webhook"
-
-        headers = {
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {env.META_USER_TOKEN}"
-        }
-        body = {
-                    "messaging_product" :  "whatsapp",
-                    "recipient_type"    :  "individual",
-                    "to"                :  wa_id,
-                    "type"              :  "text",
-                    "text"              :  { "preview_url" : True,
-                        "body" : response_json }
-        }
-
-        options = {
-               "body": json.dumps(body),
-               "method": "POST",
-               "headers": {
-                 "Authorization": f"Bearer {env.META_USER_TOKEN}",
-                 "content-type": "application/json;charset=UTF-8"
-               },
-        }
-
-        response = await fetch(uri, to_js(options))
-        console.log(f"response {response}")
-        content_type, result = await gather_response(response)
-        return Response.new( response_json, status="200")
+        if 'messages' in value:
+            console.log("Es un mensaje")
+        if 'contacts' in value:
+            console.log("Tiene contactos")
+        if 'interactive' in value:
+            console.log("Es la respuesta a un flow")
+        else:
+           await flow_reply_processor( request, env)
+        return Response.new('ok', status="200")
+        """
         try:
                 mensaje = value.messages[0].text.body 
                 console.log(f"mensaje {mensaje}")
@@ -252,8 +224,7 @@ async def on_fetch(request, env):
 
         except:
                 return Response.new('ok', status="200")
-
-       """
+        """
 
 async def send(mensaje, env):
         console.log(f"En send {mensaje}")

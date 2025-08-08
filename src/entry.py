5b062f289c36b7f276a71374f030f995eb05ce5d
alectrico <ventas@alectrico.cl>
2025-08-08 19:39:06 -0400

    dd

--- src/entry.py
+++ src/entry.py
@@ -100,12 +100,12 @@ async def on_fetch(request, env):
         #esponse = await post_tbk(pago_url, env)
         #espuesta = Response.redirect(pago_url, 307)
 
-        #html = f"<!DOCTYPE html> <html lang='es-CL' prefix='og: http://ogp.me/ns#'  > <head> <meta charset='UTF-8'> <meta http-equiv='X-UA-Compatible' content='IE=edge'> </head> <body> <form method='post' action={uri}> <input type='hidden' name='token_ws' value={token} /> <input type='submit' value='Ir a pagar' /> </form> </body>"
+        html = f"<!DOCTYPE html> <html lang='es-CL' prefix='og: http://ogp.me/ns#'  > <head> <meta charset='UTF-8'> <meta http-equiv='X-UA-Compatible' content='IE=edge'> </head> <body> <form method='post' action={uri}> <input type='hidden' name='token_ws' value={token} /> <input type='submit' value='Ir a pagar' /> </form> </body>"
 
         headers = {"content-type": "text/html;charset=UTF-8" }
         #response = Response.new( html, headers=headers )
 
-        return Response.new('<!DOCTYPE html><html lang="en"><head><title>Cloudflare Worker HTML</title></head><body><h1>Hello from Cloudflare Worker!</h1></body></html>', {  "headers": headers  })
+        return Response.new(html, {  "headers": headers  })
         #/Se inicia un buffer header para que sea lleando en agendar y usado en pagar
         #uy_order = await REPAIR_ALECTRICO.get("last_id")
         #ew_buy_order = Number(buy_order) + 1

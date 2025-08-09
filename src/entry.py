b73996ac8cfdded9fab5fde7b2de8bf8db0ad0ef
alectrico <ventas@alectrico.cl>
2025-08-09 03:13:39 -0400

    dd

--- src/entry.py
+++ src/entry.py
@@ -355,7 +355,7 @@ async def flow_reply_processor(request_json, env):
 
 
 async def mostrar_formulario_de_pago(request, buy_order, amount):
-  html = "<!DOCTYPE html>
+  html = f"<!DOCTYPE html>
 <h:tml lang='es-CL' prefix='og: http://ogp.me/ns#'  >
 <head>
   <meta charset='UTF-8'>

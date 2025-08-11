bc77c55117db4a69459ee974439ad2f607226b63
alectrico <ventas@alectrico.cl>
2025-08-10 20:48:46 -0400

    dd

--- src/entry.py
+++ src/entry.py
@@ -95,14 +95,15 @@ async def on_fetch(request, env):
         token_ws, uri = await genera_link_de_pago_tbk( buy_order, amount, env.RETURN_URL, buy_order, env)
         return mostrar_formulario_de_pago(request, env, buy_order, amount, uri, token_ws)
 
-    if url.path == "/return_url" and hasattr( url.path, 'token_ws'):
+
+    if url.path == "/return_url" and hasattr( params, 'token_ws'):
         token_ws = params['token_ws'][0]
         console.log(f"En return_url token_ws: {token_ws}")
         vci = tbk_commit( token_ws)
         return Response( vci , status="200")
 
 
-    if url.path == "/return_url" and hasattr( url.path, 'TBK_TOKEN'):
+    if url.path == "/return_url" and hasattr( params, 'TBK_TOKEN'):
         console.log("En return_url TKB_TOKEN {TKB_TOKEN}")
         return Response('ok', status="200")
 

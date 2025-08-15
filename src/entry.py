05ab917f1b5c02f20982ac4db499d1fa8821d774
alectrico <ventas@alectrico.cl>
2025-08-15 01:11:27 -0400

    Verifica User

--- src/entry.py
+++ src/entry.py
@@ -100,8 +100,13 @@ async def on_fetch(request, env):
 
     console.log(f"Handling request {url.path} with params {params}")
 
+    if url.path == '/v/uR21SF_P0pnd8rQAMGSfEg/verifica_user'
+       return await send_msg(env, str(env.FONO_JEFE), f"Hola Jefe, alguien llegó a verifica_user" )
+
+
+
     if url.path == '/':
-        await send_msg(env, str(env.FONO_JEFE), f"Hola Jefe, alguien llegó a /" )
+        return await send_msg(env, str(env.FONO_JEFE), f"Hola Jefe, alguien llegó a /" )
 
 
     if url.path == '/agendar':
@@ -109,7 +114,7 @@ async def on_fetch(request, env):
         #buy_order  = params['buy_order'][0]
         #amount     = params['amount'][0]
         #session_id = params['session_id'][0]
-        await send_msg(env, str(env.FONO_JEFE), f"Hola Jefe, alguien escribió: agendar\n\n----" )
+        return await send_msg(env, str(env.FONO_JEFE), f"Hola Jefe, alguien escribió: agendar\n\n----" )
 
 
 

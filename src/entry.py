99e97bf35a10d870555aa9a247b3e9b4c86f64e9
alectrico <ventas@alectrico.cl>
2025-08-21 03:58:32 -0400

    Era texto

--- src/entry.py
+++ src/entry.py
@@ -117,7 +117,7 @@ async def on_fetch(request, env):
         #fono        = params['phone'][1]
         #descripcion = params['message'][1]
 
-        await say_jefe( env, f"en create_from_landing_page Name {params['data'][0][1]}")
+        await say_jefe( env, f"en create_from_landing_page Name {texto['data']}")
         reply   = (
                     f"*buy_order*    { buy_order}     \n"
                     f"*amount*       { amount}        \n"

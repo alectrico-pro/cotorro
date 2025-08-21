4adc05ab18d4003803b7112d4cd6133d2a5eb59e
alectrico <ventas@alectrico.cl>
2025-08-20 23:41:42 -0400

    demn

--- src/entry.py
+++ src/entry.py
@@ -103,9 +103,10 @@ async def on_fetch(request, env):
     if url.path == '/create_from_landing_page' and method== 'POST':
         console.log(f"Params en /create_from_landing_page {params}")
 
-        console.log( f"request {request.text()}")
+        texto = await request.text()
+        console.log( f"request {texto}")
 
-        await say_jefe( env, f"en create_from_landing_page {request.text()}")
+        await say_jefe( env, f"en create_from_landing_page {texto}")
 
         buy_order   = str( random.randint(1, 10000))
         amount      = env.AMOUNT

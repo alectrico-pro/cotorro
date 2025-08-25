92f74b437f5138631463d22110041f8340c38139
alectrico <ventas@alectrico.cl>
2025-08-25 11:07:47 -0400

    No llamando a say_tomar

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -145,8 +145,8 @@ async def on_fetch(request, env):
 
         token_ws, uri = await genera_link_de_pago_tbk( buy_order, amount, env.RETURN_URL, session_id, env)
 
-        await say_tomar(env, str(env.FONO_JEFE), name, direccion, comuna )
-        await say_tomar_buy_order(env, str(env.FONO_JEFE), name, direccion, comuna, buy_order)
+        #await say_tomar(env, str(env.FONO_JEFE), name, direccion, comuna )
+        #await say_tomar_buy_order(env, str(env.FONO_JEFE), name, direccion, comuna, buy_order)
 
         path_de_pago = f"/transbank?amount={env.AMOUNT}&session_id={fono}&buy_order={buy_order}"
         await say_link_de_pago( env, fono, name, descripcion, comuna, path_de_pago )

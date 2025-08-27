7480fda1c5fd7abb237f202d3e3d2411452fc039
alectrico <ventas@alectrico.cl>
2025-08-27 08:45:30 -0400

    dneuvo

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -124,7 +124,7 @@ async def guardar_message_id( env, json_response, tipo):
     id = json_response.messages[0].id 
     console.log(f"id {id}")
     #status = await env.BUY_ORDER.get(id )
-    status = await env.BUY_ORDER.GET('wamid.HBgLNTY5ODEzNzAwNDIVAgARGBJENEZDNkZGNzY4ODM5NDE2QzAA')
+    status = await env.BUY_ORDER.get('wamid.HBgLNTY5ODEzNzAwNDIVAgARGBJENEZDNkZGNzY4ODM5NDE2QzAA')
     console.log(f"status {status}")
     match status:
         case 'failed':

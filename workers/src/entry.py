199a3da3d1b6506609970d3d70851c18165e3618
alectrico <ventas@alectrico.cl>
2025-08-27 08:49:48 -0400

    denuevo

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -123,8 +123,8 @@ async def guardar_message_id( env, json_response, tipo):
     await env.BUY_ORDER.put( json_response, tipo, { 'expirationTtl': env.SEGUNDOS_DE_EXPIRACION } )
     id = json_response.messages[0].id 
     console.log(f"id {id}")
-    #status = await env.BUY_ORDER.get(id )
-    status = await env.BUY_ORDER.get('wamid.HBgLNTY5ODEzNzAwNDIVAgARGBJENEZDNkZGNzY4ODM5NDE2QzAA')
+    status = await env.BUY_ORDER.get(str(id) )
+    #status = await env.BUY_ORDER.get('wamid.HBgLNTY5ODEzNzAwNDIVAgARGBJENEZDNkZGNzY4ODM5NDE2QzAA')
     console.log(f"status {status}")
     match status:
         case 'failed':

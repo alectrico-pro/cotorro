103fd6ac5944c4af009e1ecf7e427379b3fe3ecb
alectrico <ventas@alectrico.cl>
2025-08-27 08:42:44 -0400

    denuevo

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -120,7 +120,7 @@ async def enviar_template_say_visita_flow_reserva( request, env, fono):
 
 #----------------------------- llegada de requests --------------------
 async def guardar_message_id( env, json_response, tipo):
-    await env.BUY_ORDER.put( json.dumps(json_response), tipo, { 'expirationTtl': env.SEGUNDOS_DE_EXPIRACION } )
+    await env.BUY_ORDER.put( json_response, tipo, { 'expirationTtl': env.SEGUNDOS_DE_EXPIRACION } )
     id = json_response.messages[0].id 
     console.log(f"id {id}")
     #status = await env.BUY_ORDER.get(id )

5178129fe9d17209dcb0bc8bc46c085866d3affa
alectrico <ventas@alectrico.cl>
2025-08-28 05:20:17 -0400

    Ud.

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -336,7 +336,7 @@ async def on_fetch(request, env):
                            #f"link_de_pago: {link_de_pago} {resultado}\n\n")
                            #await send_msg(env, env.FONO_JEFE, msg)
                            path_de_pago = f"/transbank?amount={env.AMOUNT}&session_id={wa_id}&buy_order={buy_order}"
-                           await say_link_de_pago( env, wa_id, name, amount, path_de_pago )
+                           await say_link_de_pago( env, wa_id, 'Ud.', amount, path_de_pago )
                            return Response( "ok", status="200")
             return Response( "ok", status="200")
 

1b747118526e673563c9aa72faf3e5e173526109
alectrico <ventas@alectrico.cl>
2025-08-29 06:52:00 -0400

    Usando FINANCIERO

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -690,9 +690,12 @@ async def say_tomar( env, wa_id, nombre, descripcion, comuna ):
 async def difundir_a_colaboradores(env, buy_order, name, descripcion, comuna, fono, email, direccion, amount):
         token_ws, uri = await genera_link_de_pago_tbk( buy_order, amount, env.RETURN_URL, email, env)
         await guardar_pedido(env, buy_order, fono, name, email, direccion, comuna, descripcion,  amount )
-        lista_string = await env.BUY_ORDER.get('colaboradores')
+        lista_string = await env.FINANCIERO.get('colaboradores')
+        console.log(f"lista_string {lista_string}")
         lista   = json.loads( lista_string)
+        console.log(f"lista {lista}")
         for colaborador in lista:
+           console.log(f"colaborador {colaborador}")
            await say_atender(env, colaborador.fono, colaborador.nombre, descripcion, comuna, buy_order)
         path_de_pago = f"/transbank?amount={env.PRECIO_TOKEN}&session_id={fono}&buy_order={buy_order}"
         return

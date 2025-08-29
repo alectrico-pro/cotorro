9c327c9c84559e9ddf96fd48270814cf7c8abae0
alectrico <ventas@alectrico.cl>
2025-08-29 00:41:37 -0400

    dneuvo

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -292,11 +292,7 @@ async def on_fetch(request, env):
                except:
                  pass
 
-
-               try:
-                 difundir(env, buy_order, 'no-indica', 'no-indica', 'no-indica', wa_id, 'user@alectrico.cl', 'no-indica', env.AMOUNT)
-               except:
-                 pass
+               await difundir(env, buy_order, 'no-indica', 'no-indica', 'no-indica', wa_id, 'user@alectrico.cl', 'no-indica', env.AMOUNT)
 
                #no puedo difundir aquí porque el cliente no ha introducido datos
                #envío al cuestionario flow para obtener los datos

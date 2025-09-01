7b5b409acfd199ab273ab588058aecc7d608a439
alectrico <ventas@alectrico.cl>
2025-09-01 07:59:56 -0400

    dneuvo

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -185,7 +185,7 @@ async def on_fetch(request, env):
         lista = await env.FINANCIERO.list(prefix = f"{fono}:token:pagado:no_expirado")
 
         if len(lista.keys) > 0:
-          key_de_token_mas_antiguo = list.keys.sort(reverse=True)[0]
+          key_de_token_mas_antiguo = lista.keys.sort(reverse=True)[0]
           token = await env.FINANCIERO.get( key_de_token_mas_antiguo.name )
           try:
             await env.FINANCIERO.delete( key_de_token_mas_antiguo.name)

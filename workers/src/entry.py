09903b457bdd2e0fac526ce166054ceb6baf629e
alectrico <ventas@alectrico.cl>
2025-09-01 08:25:12 -0400

    dnuevo

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -183,10 +183,9 @@ async def on_fetch(request, env):
         buy_order = params['buy_order'][0]
         fono = await get_fono_cliente( env, buy_order)
         lista = await env.FINANCIERO.list(prefix = f"{fono}:token:pagado:no_expirado")
-        claves = lista.keys
-        if len(claves) > 0:
+        if len(lista.keys) > 0:
           console.log(f"lista.keys {lista.keys}")
-          key_de_token_mas_antiguo = claves.sort( key = lambda token_key: token_key.name, reverse=True)[0]
+          key_de_token_mas_antiguo = lista.keys.sort( key = lambda Object: Object.name, reverse=True)[0]
           token = await env.FINANCIERO.get( key_de_token_mas_antiguo.name )
           try:
             await env.FINANCIERO.delete( key_de_token_mas_antiguo.name)

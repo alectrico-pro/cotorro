14b20fd308045db076d34f014ad38598d997065a
alectrico <ventas@alectrico.cl>
2025-09-02 06:17:42 -0400

    dneuvo

--- workers/financiero/src/entry.py
+++ workers/financiero/src/entry.py
@@ -362,8 +362,8 @@ async def save_text_message( env, id, fono, buy_order, descripcion, amount ):
 async def anotar_tokens( env, buy_order, fono, amount, cantidad ):
     now = datetime.now()
     fecha_en_el_vencimiento = now + timedelta(days = env.VENCIMIENTO_TOKEN_DIAS)
-    for numero in range(1, cantidad + 1 ):
-      pedido = { 'token': {'orden': numero, 'expira_en': str(fecha_en_el_vencimiento), 'buy_order': buy_order, 'fono': fono, "amount": amount, "acuñado_en": json.dumps( date.today().isoformat()) }}
+    for orden in range(1, cantidad + 1 ):
+      pedido = { 'token': {'orden': orden, 'expira_en': str(fecha_en_el_vencimiento), 'buy_order': buy_order, 'fono': fono, "amount": amount, "acuñado_en": json.dumps( date.today().isoformat()) }}
       await env.FINANCIERO.put( f"{fono}:{buy_order}:token:{ datetime.timestamp(now)}:{orden}", json.dumps(pedido), { 'expirationTtl': env.SEGUNDOS_DE_EXPIRACION })
     return
 

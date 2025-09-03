4f60683e020fbe1f16a47431a43aa6a1ee0900d8
alectrico <ventas@alectrico.cl>
2025-09-03 04:26:03 -0400

    dneuvo

--- workers/financiero/src/entry.py
+++ workers/financiero/src/entry.py
@@ -161,7 +161,7 @@ async def on_fetch(request, env):
              console.log("Hay colaboradores registrados")
           for key in colaboradores.keys:
              console.log(f"{key.name}")           
-             colaborador_json = it env.NOMINA.get( key.name )
+             colaborador_json = await env.NOMINA.get( key.name )
              colaborador = json.loads( colaborador_json )
              return success_mostrar_fono( env, colaborador.nombre, colaborador.fono)
         except:

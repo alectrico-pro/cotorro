89f50d547517bc42b6f0dd6068dde4d833b59351
alectrico <ventas@alectrico.cl>
2025-10-13 02:09:17 -0300

    Anotando los procedemientos crypto

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -43,6 +43,45 @@ from cryptography.hazmat.primitives.asymmetric.padding import OAEP, MGF1, hashes
 from cryptography.hazmat.primitives.ciphers import algorithms, Cipher, modes
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 
+def decrypt_request(encrypted_flow_data_b64, encrypted_aes_key_b64, initial_vector_b64):
+    flow_data = b64decode(encrypted_flow_data_b64)
+    iv = b64decode(initial_vector_b64)
+
+    # Decrypt the AES encryption key
+    encrypted_aes_key = b64decode(encrypted_aes_key_b64)
+    private_key = load_pem_private_key(
+        PRIVATE_KEY.encode('utf-8'), password=None)
+    aes_key = private_key.decrypt(encrypted_aes_key, OAEP(
+        mgf=MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+
+    # Decrypt the Flow data
+    encrypted_flow_data_body = flow_data[:-16]
+    encrypted_flow_data_tag = flow_data[-16:]
+    decryptor = Cipher(algorithms.AES(aes_key),
+                       modes.GCM(iv, encrypted_flow_data_tag)).decryptor()
+    decrypted_data_bytes = decryptor.update(
+        encrypted_flow_data_body) + decryptor.finalize()
+    decrypted_data = json.loads(decrypted_data_bytes.decode("utf-8"))
+    return decrypted_data, aes_key, iv
+
+
+def encrypt_response(response, aes_key, iv):
+    # Flip the initialization vector
+    flipped_iv = bytearray()
+    for byte in iv:
+        flipped_iv.append(byte ^ 0xFF)
+
+    # Encrypt the response data
+    encryptor = Cipher(algorithms.AES(aes_key),
+                       modes.GCM(flipped_iv)).encryptor()
+    return b64encode(
+        encryptor.update(json.dumps(response).encode("utf-8")) +
+        encryptor.finalize() +
+        encryptor.tag
+    ).decode("utf-8")
+
+
+#------------------------------------------------ FLOW ENDPOINT ---
 
 #from clips import Environment, Symbol
 

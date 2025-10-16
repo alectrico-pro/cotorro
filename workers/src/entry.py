34d9ec2fe2c67b7cc21f2582e93974540433d639
alectrico <ventas@alectrico.cl>
2025-10-16 08:15:53 -0300

    dndn

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -622,7 +622,7 @@ async def on_fetch(request, env):
                if await es_colaborador(env, wa_id):
                   console.log(f"{wa_id} es colaborador")
 
-                  stream = await env.AI.run("@cf/meta/llama-guard-3-8b", {messages: [{ role: "user", content: "hola"},{ role: "assistant", content: "¡Hola! ¿Cómo estás? ¿En qué puedo ayudarte?"} ],});
+                  stream = await env.AI.run("@cf/meta/llama-guard-3-8b", to_js( { 'messages': [{ 'role': 'user', 'content': 'hola'},{ 'role': 'assistant', 'content': '¡Hola! ¿Cómo estás? ¿En qué puedo ayudarte?'} ],} ) );
 
 
                   await send_reply(env, wa_id, reply )

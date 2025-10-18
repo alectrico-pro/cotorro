aaa73da5a27e78346c775162d3a966bc06945175
alectrico <ventas@alectrico.cl>
2025-10-18 01:41:37 -0300

    ddddff

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -749,7 +749,7 @@ async def on_fetch(request, env):
                         dico = json.dumps( {'messages': [ { 'role': 'gerente', 'content': presentacion },
                                                           { 'role': 'colaborador', 'content': descripcion }], })
 
-                        result = await env.AI.run(await env.I.get('MODELO'), dico ) 
+                        result = await env.AI.run(await env.I.get('MODELO'), to_js (dico) ) 
 
                         mensaje_colaborador =  json.dumps( { 'role': 'colaborador', 'content': result.response })
                         await env.DIALOGO.put( str(fono) + ":colaborador", mensaje_colaborador )

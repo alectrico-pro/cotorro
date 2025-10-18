a1c18de579baf581ce9ccc15d4e8063490ceea8a
alectrico <ventas@alectrico.cl>
2025-10-18 03:17:47 -0300

    continuando el diálogo IA

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -50,6 +50,17 @@ from datetime import timedelta
 import os
 from base64 import b64decode, b64encode
 
+
+from fpdf import FPDF
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size=12)
+pdf.cell(200, 10, txt="Go Be Great!", ln=1, align="C")
+pdf.output("C:/Temp/sample_demo.pdf")
+
+
+
+
 from itertools import count
 
 id_generator = count(start=1)  # Starts from 1, increments by default
@@ -769,7 +780,10 @@ async def on_fetch(request, env):
                         await send_reply(env, wa_id,  reply )
                       else:
                         console.log(f"Hay {k} mensajes en DIALOGO")
-                        
+                        mensajes = []
+                        for mensaje in await env.DIALOGO.list( prefix = f"{fono}:" )
+                          mensajes.append( mensaje )
+                        console.log(f"mensajes {mensajes}")
 
                     return Response( "Es Colaborador", status="200")
                else:

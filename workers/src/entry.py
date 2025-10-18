fb88e80473113a2b14330fd2b934695eeb8efbb3
alectrico <ventas@alectrico.cl>
2025-10-18 03:28:58 -0300

    dddddddddddd

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -51,12 +51,12 @@ import os
 from base64 import b64decode, b64encode
 
 
-from fpdf import FPDF
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font("Arial", size=12)
-pdf.cell(200, 10, txt="Go Be Great!", ln=1, align="C")
-pdf.output("C:/Temp/sample_demo.pdf")
+#from fpdf import FPDF
+#pdf = FPDF()
+#pdf.add_page()
+#pdf.set_font("Arial", size=12)
+#pdf.cell(200, 10, txt="Go Be Great!", ln=1, align="C")
+#pdf.output("C:/Temp/sample_demo.pdf")
 
 
 

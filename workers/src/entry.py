274970c8a23768fb26cb54425877aced8f13ed6b
alectrico <ventas@alectrico.cl>
2025-10-17 22:37:23 -0300

    depn

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -744,7 +744,7 @@ async def on_fetch(request, env):
                         result = await env.AI.run(await env.I.get('MODELO'), to_js(
                         { 'messages': [ mensaje_inicial, mensaje_colaborador],} ) )
 
-                        mensaje_colaborador = to_js( { 'role': 'colaborador', 'content': result.response }
+                        mensaje_colaborador = to_js( { 'role': 'colaborador', 'content': result.response })
                         await env.DIALOGO.put( str(fono)":colaborador", mensaje_colaborador )
 
 

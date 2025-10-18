dd65a65f07d09bf353b6012eda7ecbb4bcaf0256
alectrico <ventas@alectrico.cl>
2025-10-17 22:32:03 -0300

    Enlomismo

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -737,8 +737,8 @@ async def on_fetch(request, env):
                         presentacion = "Te llamas Alexander Espinosa y eres Gerente de una empresa que contacta a las personas con electricistas a domicilio. La empresa se llama alectrico Spa y posee una plataforma llamada alectrico repair. Los electricistas suscritos a la plataforma alectrico® repair revisan los avisos de personas con problemas eléctricos y pueden atenderlo a Ud. si antes han comprado tokens. IMPORTANTE:Los clientes deben escribir No para dejar de recibir mensajes."
                         mensaje_inicial     = to_js( { 'role': 'gerente', 'content': presentacion } )
                         mensaje_colaborador = to_js( { 'role': 'colaborador', 'content': descripcion } )
-                        await env.DIALOGO.put( str(fono)":gerente",     mensaje_inicial )
-                        await env.DIALOGO.put( str(fono)":colaborador", mensaje_colaborador )
+                        await env.DIALOGO.put( str(fono) + ":gerente",     mensaje_inicial )
+                        await env.DIALOGO.put( str(fono) + ":colaborador", mensaje_colaborador )
 
 
                         result = await env.AI.run(await env.I.get('MODELO'), to_js(

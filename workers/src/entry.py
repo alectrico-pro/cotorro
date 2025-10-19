0d0886051422b096f0dc0544b9a09854428022df
alectrico <ventas@alectrico.cl>
2025-10-18 22:09:10 -0300

    Mejorando la presentación
    1.Al usar role assistant eres alexo no funciona,cree que es LLama

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -770,7 +770,7 @@ async def on_fetch(request, env):
                         await env.DIALOGO.put( str(fono) + str(datetime.now()) + ":alexo" ,     mensaje_inicial )
                         await env.DIALOGO.put( str(fono) + str(datetime.now()) + ":user" , mensaje_colaborador )
                        
-                        dico =  {'messages': [ { 'role': 'assistant', 'content': presentacion },
+                        dico =  {'messages': [ { 'role': 'system', 'content': presentacion },
                                                { 'role': 'user', 'content': descripcion }], }
 
                         result = await env.AI.run(await env.I.get('MODELO'), to_js (dico) ) 

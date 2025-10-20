bd4bb35d896b3e6739d2f64662e2659c72e4361f
alectrico <ventas@alectrico.cl>
2025-10-20 04:08:04 -0300

    Call_tools en preparación

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -802,7 +802,8 @@ async def on_fetch(request, env):
                           result = await env.AI.run(await env.I.get('MODELO'), to_js (dico_con_tools ) ) 
                           if result and hasattr( result, 'tool_calls'):
                             console.log(f"Tiene tool_calls")
-                            console.log(f"{result.tool_class[0]}")
+                            for call in result.tool_calls:
+                              console.log(f"nombre de call {call.name}")
                           else:
                             console.log("No dió resultado")
 

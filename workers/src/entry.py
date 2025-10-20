4beb0f7f240586d47cb84a1256c9a41e4d0be610
alectrico <ventas@alectrico.cl>
2025-10-20 00:07:10 -0300

    Quitando stream, porque no genera result con response

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -844,7 +844,6 @@ async def on_fetch(request, env):
                         console.log(f"mensajes {mensajes}")
                         result = await env.AI.run( await env.I.get('MODELO'), to_js(
                          {
-                          'stream': True,
                           'max_tokens': 502,
                           'messages': mensajes ,} )) 
                         console.log(f"{result.response}")

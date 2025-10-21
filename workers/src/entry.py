8aea10d09c594ea06196df530dc06c6bcc61586d
alectrico <ventas@alectrico.cl>
2025-10-20 23:16:50 -0300

    tooks no en el llamado inicial

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -816,9 +816,9 @@ async def on_fetch(request, env):
                          'max_tokens': 502,
                          'messages': [ { 'role': 'system', 'content': presentacion },
                                        { 'role': 'user',   'content': descripcion }]}
-
+                        #No llamar con tools porque genera datos artificiales y luego llama
                         try:
-                          result = await env.AI.run(await env.I.get('MODELO'), to_js (dico_con_tools ) ) 
+                          result = await env.AI.run(await env.I.get('MODELO'), to_js (dico ) ) 
                           if result and hasattr( result, 'tool_calls'):
                             console.log(f"Tiene tool_calls")
                             for call in result.tool_calls:

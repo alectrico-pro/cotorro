755cf7ea80f35bab66efe7e8bb4e52885d93ceb7
alectrico <ventas@alectrico.cl>
2025-10-21 03:43:12 -0300

    No haga suposiciones sobre los valores

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -790,6 +790,7 @@ async def on_fetch(request, env):
                         dico =  {
                          'max_tokens': 502,
                          'messages': [ { 'role': 'system', 'content': presentacion },
+                                       { 'role': 'system', 'content': 'No haga supocisiones sobre los valores, pregunte si es necesita aclararlos.' },
                                        { 'role': 'user',   'content': descripcion }]}
 
                         result = await env.AI.run(await env.I.get('MODELO'), to_js (dico ) )

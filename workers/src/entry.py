2266bb75a8ed55d850ac7448d075bae4e3e4025d
alectrico <ventas@alectrico.cl>
2025-10-21 02:46:18 -0300

    Errorcito

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -374,7 +374,7 @@ async def enviar_aviso( env, nombre, telefono, email, direccion, comuna, descrip
 
   await send_reply( env, telefono, reply)
   amount = env.PRECIO_VISITA
-  await difundir_a_colaboradores(env, buy_order, nombre, descripcion, comuna, telefono, email, direccion, amount):
+  await difundir_a_colaboradores(env, buy_order, nombre, descripcion, comuna, telefono, email, direccion, amount)
 
   return f"Difusión exitosa."
 #----------------------------- WORKER ENTRYPOINT --------------------

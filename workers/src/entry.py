15d3a26e5de37a9a6568b85cfcdf88e763504e30
alectrico <ventas@alectrico.cl>
2025-10-21 23:31:16 -0300

    Mejorando la contingencia antes la no entrega de cuestionario

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -1166,6 +1166,10 @@ async def on_fetch(request, env):
                              "Solo pida lo que necesita e intentaremos avisar a los electricistas. \n"
                            )
                            await send_reply(env, wa_id, reply)
+                           mensaje_reply= { 'role': 'tool', 'content': "Error: El cuestionario no pudo ser entregado porque no es compatible con su celular." }
+                           await env.DIALOGO.put( str(fono) + ":tool" + str(datetime.now()) + ":tool",     mensaje_reply )
+
+
                            #try:
                            #  await guardar_pedido( env, buy_order, wa_id, name, email, direccion, comuna, descripcion, amount)
                            #except:

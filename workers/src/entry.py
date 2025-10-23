31354fcbc35e24d474b8bf6472af9329339f4292
alectrico <ventas@alectrico.cl>
2025-10-23 04:51:36 -0300

    Formato de llamada tradicional

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -981,39 +981,31 @@ async def on_fetch(request, env):
                                        {      'name': 'enviar_aviso',
                                        'description': 'Avisar a electricistas.',
                                'parameters': { 'type': 'object',
-                                         'properties':
-                                                   {'nombre'  :
-                                                           {'type': 'string',
+                                         'properties': {'nombre': {'type': 'string',
                                                        "minLength": 1,
                                                          'default': nombre,
-                                                     'description': "Nombre de la personas que recibirá al electricista"}},
-                                                    {'telefono':
-                                                           {'type': 'string',
+                                                     'description': "Nombre de la personas que recibirá al electricista"},
+                                                       'telefono': {'type': 'string',
                                                        "minLength": 1,
                                                          'default': fono,
-                                                     'description': 'Teléfono de contacto al que debe llamar el electricista.'}},
-                                                     {  'email':
-                                                           {'type': 'string',
+                                                     'description': 'Teléfono de contacto al que debe llamar el electricista.'},
+                                                           'email': {'type': 'string',
                                                        "minLength": 1,
                                                          'default': 'user@alectrico.cl',
-                                                     'description': 'Dirección de correo electróncao para recibir el contrato y cualquier otra documentación.'}},
-                                                     {  'direccion':
-                                                           {'type': 'string',
+                                                     'description': 'Dirección de correo electróncao para recibir el contrato y cualquier otra documentación.'},
+                                                       'direccion': { 'type': 'string',
                                                        "minLength": 1,
                                                          'default': 'no-indica',
-                                                          'comuna': 'Dirección del usuario.'}},
-
-                                                     {    'comuna':
-                                                           {'type': 'string',
+                                                          'comuna': 'Dirección del usuario.'},
+                                                          'comuna': {'type': 'string',
                                                        "minLength": 1,
                                                          'default': 'Providencia', 
-                                                          'comuna': 'Comuna hacia donde se debe dirigir el electricista'}},
-                                                    { 'description':
-                                                           {'type': 'string',
+                                                          'comuna': 'Comuna hacia donde se debe dirigir el electricista'},
+                                                     'description': 'type': 'string',
                                                        "minLength": 1,
-                                                          'default': descripcion,
-                                                     'description': 'Descripción del problema.'}}},
-                                           'required': ['nombre', 'telefono', 'email', 'direccion', 'comuna', 'descripcion' ]
+                                                         'default': descripcion,
+                                                     'description': 'Descripción del problema.'}},
+                                                        'required': ['nombre', 'telefono', 'email', 'direccion', 'comuna', 'descripcion' ]
                             }
                           ]
                         }

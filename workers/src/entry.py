0dca1716d9b8710ad48255f03e148f7f7b1e7b5c
alectrico <ventas@alectrico.cl>
2025-10-26 09:20:40 -0300

    Usando """ para la inducción

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -945,7 +945,7 @@ async def on_fetch(request, env):
                         #Guía para cuando se use LLAMA
                         #REF: https://www.llama.com/docs/how-to-guides/prompting/
 
-                        presentacion="Te llamas alec y eres el asistente de la plataforma alectrico, la cual contacta a las personas con electricistas a domicilio. Debes llenar una ficha con los siguientes datos: 
+                        presentacion=""""Te llamas alec y eres el asistente de la plataforma alectrico, la cual contacta a las personas con electricistas a domicilio. Debes llenar una ficha con los siguientes datos: 
 *FICHA*
 
 1 Nombre: Nombre de la persona que recibirá al electricista, 
@@ -955,7 +955,7 @@ async def on_fetch(request, env):
 5 Fono: Teléfono de contacto al que debe llamar el electricista, 
 6 email: Dirección de correo electrónico para recibir el contrato y cualquier otra documentación. 
 
-Cuando tengas la ficha completa, debes mostrársela al cliente para que confirme los datos. El usuario podría volver a ingresar los datos si encuentra errores. IMPORTANTE: No llames a la función enviar aviso si no tienes, al menos, el Nombre y la Descripción."
+Cuando tengas la ficha completa, debes mostrársela al cliente para que confirme los datos. El usuario podría volver a ingresar los datos si encuentra errores. IMPORTANTE: No llames a la función enviar aviso si no tienes, al menos, el Nombre y la Descripción."""
 
                         mensaje_inicial     = json.dumps( { 'role': 'system', 'content': presentacion } )
                         mensaje_colaborador = json.dumps( { 'role': 'user', 'content': descripcion } )

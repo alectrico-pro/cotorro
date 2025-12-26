7803d029a41f28b7e1c1c20b41b06934d4d73dd7
alectrico <ventas@alectrico.cl>
2025-12-25 21:07:54 -0300

    Denuevo

--- workers/src/entry.py
+++ workers/src/entry.py
@@ -1028,7 +1028,8 @@ async def on_fetch(request, env):
 
     elif (url.path == '/create_from_jorgitos_landing_page.json' or url.path == '/create_from_jorgitos_landing_page') and method == 'POST':
         console.log(f"En /create_from_jorgitos_landing_page.json ")
-
+        pedido = request.json()
+        console.log("request {pedido}")
         content_type, query = await gather_response(request)
         console.log(f"query {query}")
         params = parse_qs(query)
